6c79577a2df56b8efaafbd705b83eb627b0d3e15
Sergey Natalenko <sergey.natalenko@mail.ru>
2022-10-02 04:38:24 +0300

    Update

--- before.sh
+++ before.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install -y git-lfs
 sudo git lfs install
 
-git clone https://github.com/mozilla/firefox-translations-models
+git clone "https://github.com/mozilla/firefox-translations-models"
 gzip -d firefox-translations-models/models/dev/*/*
 
 
